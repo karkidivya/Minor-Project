0f2334f1c1f842d2423232d05f94453c4dd33d7b
khagendra7karki <077bct036.khagendra@pcampus.edu.np>
2023-12-07 20:20:11 +0545

    src changed to app

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link rel = 'icon' href = './favicon.ico'/>
+      </head>
+      <body className={inter.className}>
+        {children}
+      </body>
     </html>
   )
 }
