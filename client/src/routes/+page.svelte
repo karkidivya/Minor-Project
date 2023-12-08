dec3a8e318b05a565d16f0c47a5b406ab04a81ad
khagendra7karki <077bct036.khagendra@pcampus.edu.np>
2023-12-08 20:32:38 +0545

    Svelte Practice

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,3 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p>Learn how its all done under the hood</p>
+<svelte:head>
+    <title>KaamSewa</title>
+</svelte:head>
+<script>
+    let count = 0
+    function increment(){
+        count += 1;
+    }
+
+
+    $:{
+        // console.log('Hello World', count )
+        console.log('This will be logged whenever the count chages')
+    }
+</script>
+<nav>
+    <ul class = "flex flex-row">
+        <li class = "p-2">
+            <a href="/">Home</a>
+        </li>
+
+        <li  class = "p-2">
+            <a href="/about">About</a>
+        </li>
+    </ul>
+</nav>
+
+<main>
+    <button on:click = {increment} class = 'bg-blue-500 p-2 rounded-md text-[#fffff]'>
+        Clicked {count}
+        {count === 1 ? 'time' : 'times'}       
+    </button>
+</main>
+
