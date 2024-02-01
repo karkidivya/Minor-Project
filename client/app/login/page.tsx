262f72f726d5e23d7bb1ec7f809857f306927646
khagendra7karki <077bct036.khagendra@pcampus.edu.np>
2024-02-01 21:29:27 +0545

    login page created

---
+++ client/app/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "./LoginForm"
+import './styles.scss'
+export default function (){
+    return <>
+        <div className = 'login'>
+           <LoginForm />
+        </div>
+
+    </>
+}
