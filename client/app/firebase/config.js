5d8a7f518272bfde3ff213e1698d45688424fd9f
Rinku <saashish7070@gmail.com>
2024-02-25 11:41:37 +0545

    Storage Problem Solved

--- client/app/firebase/config.js
+++ client/app/firebase/config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
+import { getStorage, ref,getDownloadURL, uploadBytesResumable } from "firebase/storage";
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -10,7 +11,37 @@ const firebaseConfig = {
     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
+
 // Initialize Firebase
 let firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
+//Initialize Storage
+const storage = getStorage(app);
+const storageRef = ref(storage);
+
 export default firebase_app;
+
+export const storeImage = async (userId, file, setUploadProgressCallback) => {
+    const userStorageRef = ref(storageRef, `user/${userId}/${file.name}`);
+    console.log(userId)
+    const uploadTask = uploadBytesResumable(userStorageRef, file);
+  
+    uploadTask.on('state_changed', 
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setUploadProgressCallback(progress);
+      },
+      (error) => {
+        console.error(error);
+      },
+      () => {
+        console.log('Upload complete!');
+      }
+    );
+  
+    await uploadTask;
+  
+    const downloadURL = await getDownloadURL(userStorageRef);
+  
+    return downloadURL;
+  };
