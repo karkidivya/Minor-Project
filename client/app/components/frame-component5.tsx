2aa1350e8cc71f674ec842a98de0137f34da7774
Divya <karkidivya5@gmail.com>
2024-02-26 09:58:43 +0545

    routes added to links on /

--- client/app/components/frame-component5.tsx
+++ client/app/components/frame-component5.tsx
@@ -22,19 +22,19 @@ const FrameComponent5: NextPage = () => {
           onClick={onMainBGImageClick}
         />
         <div className={styles.text}>
-          <div className={styles.home}>
+          <div className={styles.home} onClick={() => router.push('/home')}>
             <div className={styles.home1}>Home</div>
           </div>
-          <div className={styles.customerService}>
-            <div className={styles.home2}>Customer Service</div>
+          <div className={styles.customerService} onClick={() => router.push('/customer-service')}>
+            <div className={styles.home2} >Customer Service</div>
           </div>
-          <div className={styles.about}>
-            <div className={styles.home3}>About</div>
+          <div className={styles.about} onClick={() => router.push('/about')}>
+            <div className={styles.home3} >About</div>
           </div>
-          <button className={styles.register}>
+          <button className={styles.register} onClick={() => router.push('/sign-up')}>
             <div className={styles.becomeAProParent}>
               <div className={styles.becomeAPro} />
-              <div className={styles.register1}>Register</div>
+              <div className={styles.register1} >Register</div>
             </div>
           </button>
         </div>
