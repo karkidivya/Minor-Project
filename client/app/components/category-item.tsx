22904414b91d4cc05006a567197d062f6880fecf
Divya <karkidivya5@gmail.com>
2024-03-09 23:46:32 +0545

    error corrected

--- client/app/components/category-item.tsx
+++ client/app/components/category-item.tsx
@@ -4,17 +4,17 @@ import styles from "./category-item.module.css";
 import axios from "axios";
 async function getData(){
   const backendUrl = process.env.BACKEND_URL
-  const res = await fetch(`${backendUrl}/serviceCategory`)
-  
+  const res = await fetch(`http://localhost:9000/serviceCategory`)
   return res.json()
 }
 export default async function CategoryItem(){
-  const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL
+  // const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL
   const data = await getData()
+ 
 
   return (
     <div className={styles.categoryItem}>
-      {/* {data.data.map((item: any, idx: number) =>  <Plumber key ={idx} image = {item.image} categoryName = {item.categoryName} categoryId = {item.categoryId} propHeight = "225px" />)} */}
+      {data.data.map((item: any, idx: number) =>  <Plumber key ={idx} image = {item.image} categoryName = {item.categoryName} categoryId = {item.categoryId} propHeight = "225px" />)}
     </div>
   );
 };
