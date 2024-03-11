646878d0ac9c5354e01244222600f3d2f3aab271
Rinku <saashish7070@gmail.com>
2024-03-11 21:41:12 +0545

    Login almost completed

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,146 +1,104 @@
 'use client'
-import React, { useEffect } from "react";
-import { FormEvent } from 'react';
+// Import necessary components and libraries
+import React, { FormEvent, useEffect, useState } from "react";
 import type { NextPage } from "next";
 import styles from "./login.module.css";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import Link from "next/link";
-import signIn from "../firebase/auth/signin";
 import axios from "axios";
-import {ToastContainer, toast} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const initialCredential = {email: "", password: ""}
+const initialCredential = { email: "", password: "" };
+
 const Login: NextPage = () => {
+  const [credential, setCredential] = useState(initialCredential);
 
-  const router = useRouter();
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+    setCredential((prev) => ({ ...prev, [name]: value }));
+  };
 
-  const [ credential, setCredential ] = useState(initialCredential)
+  const handleForm = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
-    const email = e.currentTarget.name
-    const password = e.currentTarget.value
-
-    setCredential( (prev) =>{
-      return { ...prev, [email]: password}
-    }) 
-  }
-
-  const handleForm = async (event: FormEvent<HTMLFormElement> ) => {
-    event.preventDefault()
-
-    // const { result, error } = await signIn(credential.email, credential.password);
-
-    // const accessToken = await result?.user.getIdToken();
-   
-    // console.log("Access Token:", accessToken);
-
-    // const fetchData = async(accessToken : string | undefined)=>{
-    //   const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKENDURL}/login`,{
-    //       headers:{
-    //           'Authorization': `Bearer ${accessToken}`
-    //       }
-    //   });
-    //   console.log(response.data);
-    // }
-    // fetchData(accessToken);
-    // console.log(result)
-    try{
-      const {data} = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/login`,credential)
-      console.log(data)
-      if(!data){
-        setCredential(initialCredential);      
+    try {
+      const { data } = await axios.post(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/login`,
+        credential
+      );
+      console.log(data);
+      if (!data) {
+        setCredential(initialCredential);
       }
-
-    }catch(e){
-      toast.error(e, {hideProgressBar: true})
+    } catch (e) {
+      toast.error(e, { hideProgressBar: true });
     }
-  }
-  useEffect(() =>{
+  };
+
+  useEffect(() => {
     toast.error("Successfully initialized", {
       hideProgressBar: true,
-    })
-  }, [])
-  
+    });
+  }, []);
+
   return (
     <div className={styles.login}>
-      <form className={styles.emailAddressFrame} onSubmit={handleForm} >
-      <h1 className={styles.kaamsewa}>KaamSewa</h1>
-      <div className={styles.emailAddress}>Email Address</div>
-      <div className={styles.passwordFrame}>
-        <input
-          className={styles.emailAddress1}
-          placeholder="Email Address"
-          type="text"
-          name = "email"
-          value = {credential.email}
-          onChange = {handleChange}
-        />
-      </div>
-      <div className={styles.password}>Password</div>
-      <div className={styles.passwordFrame1}>
-        <input
-          className={styles.password1}
-          placeholder="Password"
-          type="password"
-          name = "password"
-          value = {credential.password}
-          onChange = {handleChange}
-        />
-
-      </div>
-      <Link href = "#" className = {styles.forgetPassword}>Forgot Password</Link>
-      <Button type = "submit" variant = "contained" fullWidth >
-        Log In
-      </Button>
-      <div className={styles.googleFrame}>
-        <div className={styles.loginWith}>{`Login with `}</div>
-        <img
-          className={styles.devicongoogle}
-          loading="eager"
-          alt=""
-          src="/devicongoogle.svg"
-        />
-        <img
-          className={styles.googleFrameChild}
-          loading="eager"
-          alt=""
-          src="/group-6.svg"
-        />
-      </div>
-      <div className={styles.ifYouDoContainer}>
-        <span className={styles.ifYouDo}>{`If you do not have account `}</span>
-        <Link href = '/sign-up' className = {styles.signUp}>Sign Up</Link>
-      </div>
-      <ToastContainer />
-    </form>
+      <form className={styles.emailAddressFrame} onSubmit={handleForm}>
+        <h1 className={styles.kaamsewa}>KaamSewa</h1>
+        <div className={styles.emailAddress}>Email Address</div>
+        <div className={styles.emailAddress1}>
+          <TextField
+            placeholder="Email Address"
+            type="text"
+            name="email"
+            value={credential.email}
+            onChange={handleChange}
+            fullWidth
+          />
+        </div>
+        <div className={styles.password}>Password</div>
+        <div className={styles.passwordFrame1}>
+          <TextField
+            placeholder="Password"
+            type="password"
+            name="password"
+            value={credential.password}
+            onChange={handleChange}
+            fullWidth
+          />
+        </div>
+        <Link href="#" className={styles.forgetPassword}>
+          Forgot Password
+        </Link>
+        <Button type="submit" variant="contained" fullWidth>
+          Log In
+        </Button>
+        <div className={styles.googleFrame}>
+          <div className={styles.loginWith}>{`Login with `}</div>
+          <img
+            className={styles.devicongoogle}
+            loading="eager"
+            alt=""
+            src="/devicongoogle.svg"
+          />
+          <img
+            className={styles.googleFrameChild}
+            loading="eager"
+            alt=""
+            src="/group-6.svg"
+          />
+        </div>
+        <div className={styles.ifYouDoContainer}>
+          <span className={styles.ifYouDo}>{`If you do not have an account `}</span>
+          <Link href="/sign-up" className={styles.signUp}>
+            Sign Up
+          </Link>
+        </div>
+        <ToastContainer />
+      </form>
     </div>
   );
 };
 
 export default Login;
-
-
-/**
-  {
-    fullName: String,
-    email: String,
-    password: String,
-    profilePicture: Single Image,
-    introduction: String,
-    location: String,
-    skill: String,
-    proficiency: String,
-    availability: String,
-    preference: String,
-    company: String,
-    position: String,
-    timeOfWork: String,
-    reference: String,
-    certificate: Single Image,
-    education: String,
-    payment: String,
-  }
- */
